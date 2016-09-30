10e7cb4aeada79813ff6f50d1c2d3f7ced9f5853
Thibault Deutsch <thibault.deutsch@gmail.com>
2016-10-01 01:37:22 +0200

    Add dependency xrequestid

---
+++ vendor/github.com/pilu/xrequestid/xrequestid_middleware_test.go
@@ -0,0 +1,24 @@
+package xrequestid
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestXRequestID(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	middleware := New(16)
+	middleware.Generate = func(n int) (string, error) { return "test-id", nil }
+	middleware.ServeHTTP(recorder, req, func(w http.ResponseWriter, r *http.Request) {})
+
+	if id := req.Header.Get("X-Request-ID"); id != "test-id" {
+		t.Fatalf("Expected request X-Request-Id to be `test-id`, got `%v`", id)
+	}
+
+	if responseID := recorder.HeaderMap.Get("X-Request-ID"); responseID != "test-id" {
+		t.Fatalf("Expected response X-Request-Id to be `test-id`, got `%v`", responseID)
+	}
+}
